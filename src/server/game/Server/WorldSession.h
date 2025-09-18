bbea8c6ca9499d893bf2a279478bcb53f0f2b277
TheGhost <theghostgroup@gmail.com>
2025-09-18 16:12:33 +1200

    Core/Misc: Fix various crashes, also related to multithreading (#19012)
    * When iterating groups we have to either do it not in multithreaded context (map updates) or start with checking maps (they are guaranteed to change in single thread update).
    * Properly clear ComboPoint references on player remove
    * remove some possible references item may have when it is deleted during save.
    * Also clear all hostile references when unit is removed from map.
    
    (cherrypicked from 86da1a19bb36edf3242dafac6e45e87434ddff73)

--- src/server/game/Server/WorldSession.h
+++ src/server/game/Server/WorldSession.h
@@ -1080,7 +1080,7 @@ class TC_GAME_API WorldSession
             if (_tutorials[index] != value)
             {
                 _tutorials[index] = value;
-                _tutorialsChanged = true;
+                _tutorialsChanged |= TUTORIALS_FLAG_CHANGED;
             }
         }
         // Auction
@@ -1932,7 +1932,7 @@ class TC_GAME_API WorldSession
         std::atomic<uint32> m_clientTimeDelay;
         AccountData _accountData[NUM_ACCOUNT_DATA_TYPES];
         uint32 _tutorials[MAX_ACCOUNT_TUTORIAL_VALUES];
-        bool   _tutorialsChanged;
+        uint8   _tutorialsChanged;
         std::vector<std::string> _registeredAddonPrefixes;
         bool _filterAddonMessages;
         uint32 recruiterId;
